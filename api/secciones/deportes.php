2abcea2c6cfba61c058076fa216f812171d2e187
anto-svk <donayretf@gmail.com>
2025-10-28 10:16:16 -0500

    Update deportes.php

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Real Madrid clasifica a la final de la Champions League",
+"autor" => "Miguel Ángel Torres",
+"resumen" => "El equipo merengue venció 3-1 al Manchester City en una emocionante semifinal disputada en el Santiago Bernabéu.",
 ];
 ?>
