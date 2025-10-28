10f87a7421cf82db2679856c921710b358202bb8
anto-svk <donayretf@gmail.com>
2025-10-28 10:14:04 -0500

    aaa

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubren nueva especie de dinosaurio en la Patagonia",
+"autor" => "Dr. Carlos Mendoza",
+"resumen" => "Paleontólogos argentinos hallaron restos fósiles de un carnívoro gigante que habitó la región hace 90 millones de años",
 ];
 ?>
