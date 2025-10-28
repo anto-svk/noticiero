dfc5f02992cc509716d6a3feed0cbf8f81e00010
anto-svk <donayretf@gmail.com>
2025-10-28 10:14:46 -0500

    Update news details in contenedores.phpad

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Docker lanza nueva versión con mejoras de seguridad",
+"autor" => "Ana Martínez",
+"resumen" => "La actualización incluye parches críticos y nuevas funcionalidades para la gestión de contenedores en producción",
 ];
 ?>
