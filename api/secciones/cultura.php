5cb26928625e74051b642df455c5f66b32c2c4a8
anto-svk <donayretf@gmail.com>
2025-10-28 10:15:21 -0500

    Update cultura.php with news article details

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Museo del Prado inaugura exposición sobre el Renacimiento",
+"autor" => "Laura Fernández",
+"resumen" => "La muestra reúne más de 100 obras maestras de artistas italianos y españoles del siglo XVI",
 ];
 ?>
